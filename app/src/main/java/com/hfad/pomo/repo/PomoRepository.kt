666ca7b8f13e08f0513a9c81a2bfdeaef8dc996d
Behzod <72313937+behzod1996@users.noreply.github.com>
2020-10-07 01:30:09 +0900

    추가 Repo Add Repo

---
+++ app/src/main/java/com/hfad/pomo/repo/PomoRepository.kt
@@ -0,0 +1,58 @@
+package com.hfad.pomo.repo
+
+import com.hfad.pomo.model.Pomo
+import com.hfad.pomo.model.local.dao.PomoDao
+import com.hfad.pomo.model.local.dao.ProjectDao
+import com.hfad.pomo.model.local.dao.TaskDao
+import com.hfad.pomo.repo.converter.PomoConverter
+import java.util.*
+
+interface PomoRepository {
+
+    suspend fun getPomoById(id: Long): Pomo
+
+    suspend fun getCountPomoInPeriod(beginDate: Date, endDate: Date): Long
+
+    suspend fun getPomoInPerioid(beginDate: Date, endDate: Date): List<Pomo>
+
+    suspend fun addPomo(pomo : Pomo)
+
+    suspend fun savePomo(pomo : Pomo)
+
+    suspend fun deletePomo(pomo : Pomo)
+}
+
+class PomoRepositoryImp(private val pomoDao: PomoDao,
+                        private val taskDao : TaskDao,
+                        private val projectDao : ProjectDao,
+                        private val pomoConverter: PomoConverter
+) : PomoRepository {
+    override suspend fun getPomoById(id: Long): Pomo {
+        val getPomoDto = pomoDao.getById(id)
+        return pomoConverter.convert(getPomoDto)
+    }
+
+    override suspend fun getCountPomoInPeriod(beginDate: Date, endDate: Date): Long {
+        return pomoDao.getCountInPeriod(beginDate, endDate)
+    }
+
+    override suspend fun getPomoInPerioid(beginDate: Date, endDate: Date): List<Pomo> {
+        val pomo = pomoDao.getPomodorosInPeriod(beginDate, endDate)
+        return pomo.map { pomoConverter.convert(it) }
+    }
+
+    override suspend fun addPomo(pomo: Pomo) {
+        val pomoDto = pomoConverter.convertBack(pomo)
+        pomo.id = pomoDao.insert(pomoDto.pomoEntity)
+    }
+
+    override suspend fun deletePomo(pomo: Pomo) {
+        val pomoDto = pomoConverter.convertBack(pomo)
+        pomoDao.delete(pomoDto.pomoEntity)
+    }
+
+    override suspend fun savePomo(pomo: Pomo) {
+        val pomoDto = pomoConverter.convertBack(pomo)
+        pomoDao.update(pomoDto.pomoEntity)
+    }
+}
---
+++ app/src/main/java/com/hfad/pomo/repo/ProjectRepository.kt
@@ -0,0 +1,54 @@
+package com.hfad.pomo.repo
+
+import com.hfad.pomo.model.Project
+import com.hfad.pomo.model.local.dao.ProjectDao
+import com.hfad.pomo.repo.converter.ProjectConverter
+
+interface ProjectRepository {
+
+    suspend fun getProjectById(id: Long): Project?
+
+    suspend fun getProjectByName(projectName: String): Project?
+
+    suspend fun createProject(project: Project)
+
+    suspend fun saveProject(project: Project)
+
+    suspend fun deleteProject(project: Project)
+}
+
+class ProjectRepositoryImpl(private val projectDao: ProjectDao,
+                            private val projectConverter: ProjectConverter
+): ProjectRepository {
+
+    override suspend fun getProjectById(id: Long): Project? {
+        val projectEntity = projectDao.getById(id)
+        return projectConverter.convert(projectEntity)
+    }
+
+    override suspend fun getProjectByName(projectName: String): Project? {
+        val projectEntity = projectDao.getByName(projectName)
+        return projectConverter.convert(projectEntity)
+    }
+
+    override suspend fun createProject(project: Project) {
+        val projectEntity = projectConverter.convertBack(project)
+        projectEntity?.let {
+            project.id = projectDao.insert(it)
+        }
+    }
+
+    override suspend fun saveProject(project: Project) {
+        val projectEntity = projectConverter.convertBack(project)
+        projectEntity?.let {
+            projectDao.update(it)
+        }
+    }
+
+    override suspend fun deleteProject(project: Project) {
+        val projectEntity = projectConverter.convertBack(project)
+        projectEntity?.let {
+            projectDao.delete(it)
+        }
+    }
+}
