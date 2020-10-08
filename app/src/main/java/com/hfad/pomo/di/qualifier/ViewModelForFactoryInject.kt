6cfc4805359dd13cef83e837a462cd4920d34c1a
Behzod <72313937+behzod1996@users.noreply.github.com>
2020-10-08 09:50:44 +0900

    Add ViewModelForFactoryInject.kt

---
+++ app/src/main/java/com/hfad/pomo/di/qualifier/ViewModelForFactoryInject.kt
@@ -0,0 +1,7 @@
+package com.hfad.pomo.di.qualifier
+import javax.inject.Qualifier
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ViewModelForFactoryInject
